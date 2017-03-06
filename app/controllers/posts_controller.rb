4ef8f610f253b1cd4c3854f5a712e9c66c15c192
kai <328458211@qq.com>
2017-03-06 17:59:18 +0800

    add find_post and check_permission to postscontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -20,16 +22,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
-    @post.group = @group
 
   end
 
   def update
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
-    @post.group = @group
+
 
     if @post.update(post_params)
       redirect_to account_posts_path, notice: "update success!"
@@ -39,13 +36,21 @@ class PostsController < ApplicationController
   end
 
   def destroy
+
+    @post.destroy
+    redirect_to account_posts_path(@post), alert: "成功删除文章！"
+  end
+
+  def find_post_and_check_permission
     @group = Group.find(params[:group_id])
     @post = Post.find(params[:id])
     @post.group = @group
-    @post.destroy
-    redirect_to account_posts_path(@post)
-    flash[:alert] = "成功删除文章！"
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission!"
+    end
   end
+
   private
 
   def post_params
